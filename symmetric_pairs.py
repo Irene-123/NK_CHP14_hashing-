2dc22d61c1812bff799d38fa90dcc5e586506d32
kirti purohit <58950467+Irene-123@users.noreply.github.com>
2020-07-14 10:43:48 +0530

    Update symmetric_pairs.py

--- symmetric_pairs.py
+++ symmetric_pairs.py
@@ -1,3 +1,19 @@
+'''
+Given a list of Number pairs ,if pair(i,j) exist and a pair (j,i) also exist ,return the pair
+
+TEST CASE 
+Input:
+{ {1,3}, {2,6}, {3,5}, {3,1}, {7,4}, {8,7} }
+
+Output:
+  {3,1}
+'''  
+  
+
+
+
+
+
 def symmetric_pairs(d):
     for key in d:
         val = d[key]
